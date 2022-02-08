bcdf5f0359b8d5371414e436c33d075f0fd9f0ad
Didac Ortega <didac.ortega.do@gmail.com>
2022-02-08 12:09:12 +0100

    Added comment

--- src/main/java/com/example/springbootdemoapi/security/WebSecurityConfig.java
+++ src/main/java/com/example/springbootdemoapi/security/WebSecurityConfig.java
@@ -15,6 +15,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 
 import javax.sql.DataSource;
 
+//Prueba para el git hehe
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
